04185685693b00bf02f0d6dbfb4e5e2b20343774
Dmitry Snegirev <dim0n2023@yandex.ru>
2021-12-10 23:16:24 +0300

    secure POST /options/

--- app/routers/options.py
+++ app/routers/options.py
@@ -3,6 +3,7 @@ from typing import List
 
 from ..database import crud, schemas
 from ..dependencies import get_db
+from ..utils import auth
 
 
 router = APIRouter(prefix="/options", tags=["Options"])
@@ -14,6 +15,10 @@ async def read_options(db=Depends(get_db)):
 
 
 @router.post("/", status_code=201)
-async def create_option(option_data: schemas.OptionData, db=Depends(get_db)):
+async def create_option(
+    option_data: schemas.OptionData,
+    current_user=Depends(auth.get_current_admin),
+    db=Depends(get_db)
+):
     _ = crud.create_option(db=db, option_data=option_data)
     return Response(status_code=201)
